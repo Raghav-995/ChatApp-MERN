a8b5508b1a7564fee771341af2be4b031426126f
Raghav-995 <raghavendrapathak0@gmail.com>
2024-05-13 18:10:18 +0530

    adding data

--- frontend/src/components/chats.tsx
+++ frontend/src/components/chats.tsx
@@ -2,14 +2,34 @@
 import axios from 'axios';
 import { useEffect,useState} from 'react';
 
-export default function Chats(){
-    const [chats, setChats] = useState([]);
+interface Chat{
+    _id : String,
+    users: {name: String, email: String}[],
+    chatName: String,
+    key: String,
+}
+
+const Chats = ()=> {
+    const [chats, setChats] = useState<Chat[]>([]);
 
     var fetchChats = async () => {
-     var response = await axios.get('http://localhost:5000/apis/chat');
+     var response = await axios.get<Chat[]>('http://localhost:5000/apis/chat');
         setChats(response.data);
     }
     useEffect(()=> {fetchChats();},[]);
-    return <div>{chats.map((chat)=> <div><h2>{chat.chatName }</h2><h3>{chat.users.map((user)=>user.email)}</h3></div>)}</div>;
+     return (
+        <div>
+          <h2>Chat List</h2>
+          <ul>
+            {chats.map(chat => (
+              <li key = {chat._id} > {/* Ensure chat._id is a string or can be converted to a string */}
+                <strong>{chat.chatName}</strong> - {chat.users.map(user => user.email).join(', ')}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    
 }
 
+export default Chats;
